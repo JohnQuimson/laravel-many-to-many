5c20d450ab1dfbc2aed57b729c1a0521b3ca196b
JohnQuimson <john.quimson777@gmail.com>
2024-02-12 20:04:37 +0100

    BONUS: seed technology

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +15,16 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Laravel'];
+
+        foreach ($technologies as $technology) {
+
+            $new_technology = new Technology();
+
+            $new_technology->title = $technology;
+            $new_technology->slug = Str::of($technology)->slug('-');
+
+            $new_technology->save();
+        }
     }
 }
