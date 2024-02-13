ebdfe9e99cebfc313422a3f2d16c1c9c31a5fed6
JohnQuimson <john.quimson777@gmail.com>
2024-02-13 18:14:24 +0100

    feat: show style

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,28 +1,49 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container d-flex flex-column align-items-center">
-        <div class="header-show d-flex align-items-center gap-2">
-            <h2>{{ $project->title }}</h2>
-            <span class="card-subtitle mb-2 text-body-secondary">{{ $project->visibility }}</span>
-        </div>
-        <div class="cont-info d-flex gap-5">
-            <span>{{ $project->main_language }}</span>
-            <span>Ultima modifica: {{ $project->last_updated }}</span>
-            <span>Categoria: {{ $project->type?->title ?: 'Nessuna categoria' }}</span>
-            <div>
-                Tecnologie utilizzate
-                <ul>
-
-                    @foreach ($project->technologies as $technology)
-                        <li>
-                            {{ $technology->title }}
-                        </li>
-                    @endforeach
-                </ul>
+    <div class="main-show">
+        <h1>Card Info</h1>
+        <div class="jq-cont mt-5">
+            {{-- header --}}
+            <div class="header-show">
+                <h2>{{ $project->title }}</h2>
+                <span class="card-subtitle mb-2">{{ $project->visibility }}</span>
             </div>
-        </div>
+            {{-- Info --}}
+            <div class="cont-info d-flex flex-column gap-3">
+                {{-- first row --}}
+                <div class="riga">
+                    <div class="d-flex flex-column">
+                        <span class="text-secondary">Main language:</span>
+                        <span class="text-center">{{ $project->main_language }}</span>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <span class="text-secondary">Ultimo aggiornamento:</span>
+                        <span class="text-center">{{ $project->last_updated }}</span>
+                    </div>
+                </div>
+                {{-- second row --}}
+                <div class="riga">
+                    <div class="d-flex flex-column">
+                        <span class="text-secondary">Categoria: </span>
+                        <span class="text-center">{{ $project->type?->title ?: 'Nessuna categoria' }}</span>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <span class="text-secondary">Tecnologie utilizzate:</span>
+                        <ul>
+                            @foreach ($project->technologies as $technology)
+                                <li class="text-center">
+                                    {{ $technology->title }}
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
 
-        <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-warning">Modifica</a>
+
+            </div>
+
+            <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-warning">Modifica</a>
+        </div>
     </div>
 @endsection
